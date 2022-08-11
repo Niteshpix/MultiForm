460958235f8648e7cf737382043f44119ba49797
unknown <nitesh@pixlerlab.com>
2022-08-11 16:47:35 +0530

    SecCommit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import '../App.css';
+import { Box, Button } from '@mui/material'
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <Box className='App'>
+    <h1>Welcome to Jobeefy</h1>
+  
+    <Button variant="contained">Apply Here !!</Button>
+
+    </Box>
   )
 }
 
